c7f6fce074f79208b10abf4ff3dad8a929aa2488
Ashpreet Bedi <ashpreet@phidata.com>
2023-01-08 22:02:54 -0500

    update

--- workflows/crypto/prices_pg.py
+++ workflows/crypto/prices_pg.py
@@ -5,48 +5,38 @@ from phidata.task import TaskArgs, task
 from phidata.utils.log import logger
 from phidata.workflow import Workflow
 
-from workflows.sql_dbs import PG_DB_APP, PG_DB_CONN_ID
+from workflows.sql_dbs import POSTGRES_APP, POSTGRES_CONN_ID
 
 ##############################################################################
 # A workflow to loads daily cryptocurrency price data to a
 # postgres table: `crypto_prices_daily`
 ##############################################################################
 
-# List of coins to get prices for
-coins = [
-    "bitcoin",
-    "ethereum",
-    "litecoin",
-    "ripple",
-    "tether",
-]
-
 # Step 1: Define a postgres table for storing crypto price data
 crypto_prices_daily_pg = PostgresTable(
     name="crypto_prices_daily",
     # use the connection URL from the dev_pg_db object
-    db_app=PG_DB_APP,
+    db_app=POSTGRES_APP,
     # provide the connection ID used by airflow
-    airflow_conn_id=PG_DB_CONN_ID,
+    airflow_conn_id=POSTGRES_CONN_ID,
 )
 
-# Step 2: Create tasks to load the crypto_prices_daily_pg table
+
+# Step 2: Create tasks to load the crypto_prices_daily table
 # 2.1 Download price data
 @task
 def load_crypto_prices(**kwargs) -> bool:
     """
     Download prices and load postgres table.
     """
-    import pandas as pd
-    import requests
+    import httpx
+    import polars as pl
 
-    args: TaskArgs = TaskArgs.from_kwargs(kwargs)
-    run_date = args.run_date
-    run_day = run_date.strftime("%Y-%m-%d")
-    run_hour = run_date.strftime("%H")
+    coins = ["bitcoin", "ethereum", "litecoin", "ripple", "tether"]
+    run_date = TaskArgs.from_kwargs(kwargs).run_date
 
-    logger.info(f"Downloading prices for: ds={run_day}/hr={run_hour}")
-    response: Dict[str, Dict] = requests.get(
+    logger.info(f"Downloading prices for run_date={run_date}")
+    response: Dict[str, Dict] = httpx.get(
         url="https://api.coingecko.com/api/v3/simple/price",
         params={
             "ids": ",".join(coins),
@@ -58,18 +48,25 @@ def load_crypto_prices(**kwargs) -> bool:
         },
     ).json()
 
-    logger.info("Converting response to dataframe")
-    _df = pd.DataFrame.from_dict(response, orient="index")
-    _df.index.name = "ticker"
-    _df["ds"] = run_day
-    _df["hour"] = run_hour
-    _df["dttm"] = run_date
-    _df.reset_index(inplace=True)
-    _df.set_index(["ds", "hour", "ticker"], inplace=True)
+    # Create a dataframe from the response
+    df = pl.DataFrame(
+        [
+            {
+                "ds": run_date.date(),
+                "hour": run_date.strftime("%H"),
+                "ticker": coin_name,
+                "usd:": coin_data["usd"],
+                "usd_market_cap": coin_data["usd_market_cap"],
+                "usd_24h_vol": coin_data["usd_24h_vol"],
+                "usd_24h_change": coin_data["usd_24h_change"],
+                "last_updated_at": coin_data["last_updated_at"],
+            }
+            for coin_name, coin_data in response.items()
+        ]
+    )
 
-    print(_df.head())
-
-    return crypto_prices_daily_pg.write_pandas_df(_df, if_exists="append")
+    # Write the dataframe to table
+    return crypto_prices_daily_pg.write_df(df, if_exists="append")
 
 
 # 2.2 Drop existing price data to prevent duplicates
