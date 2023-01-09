a7ce5994984042078eb0d252da055781afef4546
Ashpreet Bedi <ashpreet@phidata.com>
2023-01-08 20:51:45 -0500

    crypto prices

--- workflows/crypto/prices.py
+++ workflows/crypto/prices.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from phidata.asset.file import File
+from phidata.asset.local.file import LocalFile
 from phidata.task import TaskArgs, task
 from phidata.utils.log import logger
 
@@ -10,22 +10,23 @@ from phidata.utils.log import logger
 
 # Step 1: Define a File object for storing crypto price data
 # Path: `storage/crypto/crypto_prices.csv`
-crypto_prices_file = File(
+crypto_prices_file = LocalFile(
     name="crypto_prices.csv",
     file_dir="crypto",
 )
 
+
 # Step 2: Create a task that downloads price data
 @task
 def download_crypto_prices(**kwargs) -> bool:
-    import pandas as pd
-    import requests
+    import httpx
+    import polars as pl
 
     coins = ["bitcoin", "ethereum"]
     run_date = TaskArgs.from_kwargs(kwargs).run_date
 
     logger.info(f"Downloading prices for run_date={run_date}")
-    response: Dict[str, Dict] = requests.get(
+    response: Dict[str, Dict] = httpx.get(
         url="https://api.coingecko.com/api/v3/simple/price",
         params={
             "ids": ",".join(coins),
@@ -37,16 +38,24 @@ def download_crypto_prices(**kwargs) -> bool:
         },
     ).json()
 
-    logger.info("Converting response to dataframe")
-    _df = pd.DataFrame.from_dict(response, orient="index")
-    _df.index.name = "ticker"
-    _df["dttm"] = run_date
-    _df.reset_index(inplace=True)
-    _df.set_index(["dttm", "ticker"], inplace=True)
+    # Create a dataframe from the response
+    df = pl.DataFrame(
+        [
+            {
+                "ds": run_date.date(),
+                "ticker": coin_name,
+                "usd:": coin_data["usd"],
+                "usd_market_cap": coin_data["usd_market_cap"],
+                "usd_24h_vol": coin_data["usd_24h_vol"],
+                "usd_24h_change": coin_data["usd_24h_change"],
+                "last_updated_at": coin_data["last_updated_at"],
+            }
+            for coin_name, coin_data in response.items()
+        ]
+    )
 
-    print(_df.head())
-
-    return crypto_prices_file.write_pandas_df(_df)
+    # Write the dataframe to file
+    return crypto_prices_file.write_df(df)
 
 
 # Instantiate the task
